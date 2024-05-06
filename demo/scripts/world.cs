ad82855fb84cecd8fca29d5d1cc32e52167e87de
ItsLever <itslatcharts@gmail.com>
2024-05-05 23:42:27 -0400

    Some setup for first algo
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- demo/scripts/world.cs
+++ demo/scripts/world.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Text;
 
 public partial class world : Node2D
 {
@@ -7,6 +8,47 @@ public partial class world : Node2D
 	public override void _Ready()
 	{
 		GD.Print("Hello World");
+		TileMap tmp = (TileMap)this.GetChild(0);
+		Create(30, 30, tmp);
+	}
+
+	private static int[,] tiles;
+	private static bool[,] vis;
+	public static void Create(int n, int m, TileMap map)
+	{
+		tiles = new int[n,m];
+		vis = new bool[n, m];
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (map.GetCellTileData(0, new Vector2I(j, i), false) == null)
+					continue;
+				//theres smth here
+				var data = map.GetCellTileData(0, new Vector2I(j, i), false);
+			//	data.
+				tiles[i, j] = 1;
+			}
+		}
+		Disp(tiles);
+	}
+
+	public static void Disp(int[,] thing)
+	{
+		for (int i = 0; i < thing.GetUpperBound(1); i++)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < thing.GetUpperBound(0); j++)
+			{
+				sb.Append(thing[i, j]);
+			}
+			GD.Print(sb.ToString());
+		}
+	}
+
+	public static void Bfs()
+	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
